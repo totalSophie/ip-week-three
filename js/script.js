96aa128ff538ffd12e80f198a2585d9a49bdbe7a
Sophie <totalsophie@gmail.com>
2023-04-24 14:48:56 +0300

    changed the script file for the hover effect to take effect

--- js/script.js
+++ js/script.js
@@ -33,34 +33,27 @@ $(document).ready(function(){
   
   $(document).ready(function(){
     $("#work1").mouseover(function(){
+      console.log("hover1")
       $("#overlay").show();
     }).mouseout(function(){
       $("#overlay").hide();
     });
-  });
-  $(document).ready(function(){
     $("#work2").mouseover(function(){
       $("#overlay2").show();
     }).mouseout(function(){
       $("#overlay2").hide();
     });
-  });
-  $(document).ready(function(){
     $("#work3").mouseover(function(){
       $("#overlay3").show();
     }).mouseout(function(){
       $("#overlay3").hide();
     });
-  });
-  $(document).ready(function(){
     $("#work4").mouseover(function(){
+      console.log("hover4")
       $("#overlay4").show();
     }).mouseout(function(){
       $("#overlay4").hide();
     });
-  });
-  
-  $(document).ready(function(){
     $("#work5").mouseover(function(){
       $("#overlay5").show();
     }).mouseout(function(){
